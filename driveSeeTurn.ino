721ee27550b196c219b8565263150102a3d36bac
Tom Bertalan <tom@tombertalan.com>
2015-02-21 15:03:12 -0500

    Some cleanup and renaming.

--- driveSeeTurn.ino
+++ driveSeeTurn.ino
@@ -1,48 +1,57 @@
-/* 
-For use witfh the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
-*/
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <SharpIR.h>
 
+// CONSTANTS
+// These will be compiled away (I think)
+// as if they were #define's, but without
+// accidentally breaking code.
 
-#define IRPIN A2
-#define IRREADINGS 64
-#define IRDIFFERENCE 93
-#define IRMODEL 20150
-SharpIR sharp(IRPIN, IRREADINGS, IRDIFFERENCE, IRMODEL);
+// IR sensor
+const int IRPIN = A2;
+const int IRREADINGS = 64;
+const int IRDIFFERENCE = 93;
+const int IRMODEL = 20150;
 
-
-
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-her 
-// Define two of four motors
-Adafruit_DCMotor *drive = AFMS.getMotor(1);
-Adafruit_DCMotor *steering = AFMS.getMotor(2);
-
-
-
-
-  
+// Motor speeds and ramp increment delays.
 const int maxSpeed = 20;
 const int driveDelay = 2;
 const int steerSpeed = 90;
 const int steerDelay = 0;
-  
-const int sightThresh = 80;
-const int backDist = 40;
-  
-const int STEPDELAY = 0;
 
-const int switchPin = 13;
-const int switchOut = A0;
-const int offDelay = 2000; // Minimum number of ms to be off
+// Distances (cm) and angles (deg) for navigation.
+const int sightThresh = 80;  // Distance within which a turn
+                             // should be executed rather than a forward run.
+//const int backDist = 40;   // Distance to backtrack for very close objects.
+const int turnAngle = 44;    // Can be anything, but will executed modulo 22
+                             // (the emperical included angle of a
+                             // single three-point turn).
+
+const int STEPDELAY = 0;  // Delay within main loop. Lower -> less sanity.
+
+// Sleep switch
+const int switchRead = 13;  // Provide digital HIGH ...
+const int switchWrite = A0; // ... to be read by the sleep switch
+                            // (two pins might not actually be necessary for this).
+const int offDelay = 2000;  // Minimum number of ms to be off
+                            // before checking the switch again.
+
+
+
+
+
+// Create IR reader object.
+SharpIR sharp(IRPIN, IRREADINGS, IRDIFFERENCE, IRMODEL);
+
+// Create the motor shield object with the default I2C address.
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Define two of four motors.
+Adafruit_DCMotor *drive = AFMS.getMotor(1);
+Adafruit_DCMotor *steering = AFMS.getMotor(2);
+
+
 
 
 
@@ -67,38 +76,22 @@ void rampDown(Adafruit_DCMotor *motor, int topSpeed, int delayTime){
 
 
 
-
-void notImplemented(String caller)
-{
-  Serial.print("NOT IMPLEMENTED:");
-  Serial.println(caller);
-}
-
-
-
-
-
-
-
-
-
-
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
 
   //AFMS.begin();  // create with the default frequency 1.6KHz
   AFMS.begin(10000);  // OR with a different frequency
-  
+
   // Set the speed to start, from 0 (off) to 255 (max speed)
   drive->setSpeed(150);
   drive->run(FORWARD);
   // turn on motor
   drive->run(RELEASE);
   steering->run(RELEASE);
-  
+
   Serial.begin(9600);
   pinMode(IRPIN, INPUT);
-  pinMode(switchPin, INPUT);
+  pinMode(switchRead, INPUT);
   pinMode(A0, OUTPUT);
   digitalWrite(A0, HIGH);
 
@@ -157,11 +150,12 @@ void go(int cmDist, bool forward)
   Serial.print("Going ");
   if(forward){
     Serial.print(" forward ");
-  }else{
+  }
+  else{
     Serial.print(" backward ");
   }
   Serial.println(msg + cmDist + " cm.");
-  
+
   //drive->run(RELEASE); // stop the motor
   if(forward)
   {
@@ -215,19 +209,19 @@ void loop() {
   uint8_t i;
   int dis;
 
-  if(digitalRead(switchPin) == HIGH && done == false)
+  if(digitalRead(switchRead) == HIGH && done == false)
   {
     dis = getDistance();
-     
+
     if(dis < sightThresh)
     {
       // If an object is ahead of us, turn 90 degrees.
-//      if(dis < backDist)
-//      {
-//        // If the object is very close, go back a little first, then turn.
-//        go(25, false);
-//      }
-      turn(44);
+      //      if(dis < backDist)
+      //      {
+      //        // If the object is very close, go back a little first, then turn.
+      //        go(25, false);
+      //      }
+      turn(turnAngle);
     }
     else
     {
@@ -237,10 +231,10 @@ void loop() {
         go(dis / 4, true);
       }
     }
-    
+
     delay(STEPDELAY);
-//    go(100, true);
-//    done = true;
+    //    go(100, true);
+    //    done = true;
   }
   else
   { // switch is off
@@ -249,4 +243,6 @@ void loop() {
     delay(offDelay);
   }
 }
+
+
 
